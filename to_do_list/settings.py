5be8b4295f5bbe20128919ee7dfaf1b69ef1c748
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-03-31 15:29:09 +0530

    Update settings.py

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
